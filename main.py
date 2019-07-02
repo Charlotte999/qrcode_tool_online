4eec8e29348ede7007fd5893de12183f0852d751
unknown <1897l>
2019-07-02 10:52:45 +0800

    优化二维码生成器

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import flask
 import qrcode
+import io
 
 app =flask.Flask(__name__)
 
@@ -17,17 +18,21 @@ def home():
 
     return flask.render_template("qr_tool.html")
 
-@app.route("/qr",methods=["POST"])
+@app.route("/qr")
 def qr():
     ##第一步：获取要生成二维码的数据
-    data = flask.request.form.get("data")
+    data = flask.request.args.get("data")
 
     ##第二步：生成二维码图像
     img = qrcode.make(data)
-    img.save(r"D:\实习培训\代码\day6\qrcode_tool_online\static\qr.png")
+    #img.save(r"D:\实习培训\代码\day6\qrcode_tool_online\static\qr.png")
+    bi = io.BytesIO() #创建一个BytesIO对象，用于在内存中存储二维码图像数据
+    img.save(bi, "png") #调用img对象的save方法将二维码图像数据以PNG编码格式写入bi对象管理的内存空间bi
+    bi.seek(0)
 
     ##第三步：在页面上显示二维码图片
-    return '<img src="/static/qr.png" />'
+    #return '<img src="/static/qr.png" />'
+    return flask.send_file(bi, "image/png")
 
 if __name__ == '__main__':
     app.run(debug=True)
